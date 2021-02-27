092362480fc62137f9a630e4854e748f55c77457
Artem <64410376+Artiss90@users.noreply.github.com>
2021-02-27 17:36:34 +0200

    Функция createReducer()

--- src/redux/contactsRedux/contactsRedux.js
+++ src/redux/contactsRedux/contactsRedux.js
@@ -1,37 +1,57 @@
+import { createReducer } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
-import { ADD_CONTACT, DELETE_CONTACT, CHANGE_FILTER } from './contactsTypes';
+import contactsAction from './contactsAction';
+// import { ADD_CONTACT, DELETE_CONTACT, CHANGE_FILTER } from './contactsTypes';
 
-const itemsRedux = (items = [], { type, payload }) => {
-  switch (type) {
-    case ADD_CONTACT: {
-      /**проверка на повторение имён */
-      if (items.find(contactName => contactName.name === payload.name)) {
-        console.log(`${payload.name} is already in contacts!`);
-        return items;
-      }
-      /**добавляем новый контакт в в состояние контактов */
-      return [...items, payload];
+const itemsRedux = createReducer([], {
+  [contactsAction.addContact]: (_, { payload }) => {
+    /**проверка на повторение имён */
+    if (_.find(contactName => contactName.name === payload.name)) {
+      console.log(`${payload.name} is already in contacts!`);
+      return _;
     }
+    /**добавляем новый контакт в в состояние контактов */
+    return [..._, payload];
+  },
 
-    case DELETE_CONTACT: {
-      /**оставляем отфильтрованные контакты, у которых не совпал id */
-      return items.filter(contact => contact.id !== payload);
-    }
+  [contactsAction.deleteContact]: (_, { payload }) =>
+    _.filter(contact => contact.id !== payload),
+});
+// const itemsRedux = (items = [], { type, payload }) => {
+//   switch (type) {
+//     case ADD_CONTACT: {
+//       /**проверка на повторение имён */
+//       if (items.find(contactName => contactName.name === payload.name)) {
+//         console.log(`${payload.name} is already in contacts!`);
+//         return items;
+//       }
+//       /**добавляем новый контакт в в состояние контактов */
+//       return [...items, payload];
+//     }
 
-    default:
-      return items;
-  }
-};
-const filterRedux = (filter = '', { type, payload }) => {
-  switch (type) {
-    case CHANGE_FILTER: {
-      return payload;
-    }
+//     case DELETE_CONTACT: {
+//       /**оставляем отфильтрованные контакты, у которых не совпал id */
+//       return items.filter(contact => contact.id !== payload);
+//     }
 
-    default:
-      return filter;
-  }
-};
+//     default:
+//       return items;
+//   }
+// };
+
+const filterRedux = createReducer('', {
+  [contactsAction.changeFilter]: (_, { payload }) => payload,
+});
+// const filterRedux = (filter = '', { type, payload }) => {
+//   switch (type) {
+//     case CHANGE_FILTER: {
+//       return payload;
+//     }
+
+//     default:
+//       return filter;
+//   }
+// };
 
 export default combineReducers({
   items: itemsRedux,
