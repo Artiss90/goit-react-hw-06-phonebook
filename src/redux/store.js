266f12d0bbf79fd33196f328232c108875a8b536
Artem <64410376+Artiss90@users.noreply.github.com>
2021-02-27 16:55:08 +0200

    Функия configureStore()

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+// import { combineReducers } from 'redux';
 import contactsRedux from './contactsRedux/contactsRedux';
 
 // {
@@ -8,13 +10,18 @@ import contactsRedux from './contactsRedux/contactsRedux';
 //   }
 // }
 
-const rootReducer = combineReducers({
-  contacts: contactsRedux,
-});
+// const rootReducer = combineReducers({
+//   contacts: contactsRedux,
+// });
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = configureStore({
+  reducer: { contacts: contactsRedux },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV === 'development',
+});
+// const store = createStore(
+//   rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// );
 
 export default store;
